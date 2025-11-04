7ae40f1404af2b38e3309e4259c344bc08940f77
ismigue23 <migueel.tomaas@gmail.com>
2025-11-04 16:03:31 +0100

    Create settings-menu.dart

---
+++ pantalla-principal/settings-menu.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class SettingsMenu extends StatelessWidget {
+  const SettingsMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              color: const Color(0xFF7BA238),
+              child: const Text(
+                "Ajustes",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: ListView(
+                children: [
+                  _menuItem(
+                    icon: Icons.restaurant_menu,
+                    text: "Administrar menú",
+                    onTap: () {},
+                  ),
+                  _menuItem(
+                    icon: Icons.person_pin,
+                    text: "Administrar roles",
+                    onTap: () {},
+                  ),
+                  _menuItem(
+                    icon: Icons.table_bar,
+                    text: "Administrar mesas",
+                    onTap: () {},
+                  ),
+                  _menuItem(
+                    icon: Icons.menu_book,
+                    text: "Editar carta",
+                    onTap: () {},
+                  ),
+                  _menuItem(
+                    icon: Icons.group,
+                    text: "Editar usuarios",
+                    onTap: () {},
+                  ),
+                  _menuItem(
+                    icon: Icons.food_bank_outlined,
+                    text: "Editar platos",
+                    onTap: () {},
+                  ),
+                  _menuItem(
+                    icon: Icons.inventory,
+                    text: "Editar inventario",
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _menuItem({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      leading: Icon(icon, size: 26),
+      title: Text(text, style: const TextStyle(fontSize: 17)),
+      onTap: onTap,
+      trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+    );
+  }
+}
